e1c836ac3861df51d95e80b8cb7ea367e79e2411
darenlee <daren.lee@mathworks.com>
2015-08-25 09:32:53 -0400

    Update my_function.m

--- AR_Drone_Target/+codertarget/+arm_cortex_a_drone/+internal/my_function.m
+++ AR_Drone_Target/+codertarget/+arm_cortex_a_drone/+internal/my_function.m
@@ -1,6 +1,6 @@
 % Copyright 2014 The MathWorks, Inc.
 function my_function()
-    ab = getActiveConfigSet(gcs);
+    ab = getActiveConfigSet(bdroot); %was gcs but this is not the best method
     IP_String  = codertarget.data.getParameterValue(ab,'IP');
     setenv('AR_DRONE_IP_ADDRESS',IP_String)
 end
