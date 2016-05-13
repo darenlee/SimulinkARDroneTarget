10b67a15e8688be760db5f8d996eef5426ae80b5
sannem <sanne.marx@mathworks.nl>
2016-05-13 13:41:08 +0200

    Re-implemented killing of old binaries in the new SSHDownload.m implementation

--- AR_Drone_Target/+codertarget/+arm_cortex_a_drone/+internal/SSHDownload.m
+++ AR_Drone_Target/+codertarget/+arm_cortex_a_drone/+internal/SSHDownload.m
@@ -14,6 +14,11 @@ disp('Opening TPCIP connection with the AR Drone');
 droneTcpip = tcpip('192.168.1.1',23);
 fopen(droneTcpip);
 
+disp(['Killing any instances of Program.elf or previously running ' fileName]);
+query(droneTcpip,'killall -9 program.elf.respawner.sh');
+query(droneTcpip,'killall -9 program.elf');
+query(droneTcpip,['killall -9' fileName]);
+
 disp('Starting the program on the AR Drone');
 query(droneTcpip,['chmod 777 /update/' fileName]);
 query(droneTcpip,['./update/' fileName ' -w']);
