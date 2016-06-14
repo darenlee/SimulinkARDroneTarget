d0de8d3184934ce7382056b5473dea8842b367a6
sannem <sanne.marx@mathworks.nl>
2016-06-14 13:04:17 +0200

    fixed connection errors if current model was not the parent model

--- AR_Drone_Target/extModeParams.m
+++ AR_Drone_Target/extModeParams.m
@@ -2,8 +2,15 @@ function [ IP varOut2 port ] = iptest( varargin )
 %IPTEST Summary of this function goes here
 %   Detailed explanation goes here
 
+%% make sure that the correct (parent) model is used for getting the configset
 
-hCs = getActiveConfigSet(gcs);
+parent = get_param(gcs,'parent');
+if isequal(parent,'');
+    hCs = getActiveConfigSet(gcs);
+else
+    hCs = getActiveConfigSet(parent);
+end
+  
 data = hCs.get_param('CoderTargetData');
 IP = data.IP;
 varOut2 = 1;
