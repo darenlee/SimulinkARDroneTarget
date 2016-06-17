3188c741b85d1f319b6d56bb036d5c5b4060f537
sannem <sanne.marx@mathworks.nl>
2016-06-17 15:39:02 +0200

    Added default sensor calibration params. moved scripts to /scripts fix #157

--- AR_Drone_Target/scripts/DroneStartup.m
+++ AR_Drone_Target/scripts/DroneStartup.m
@@ -13,17 +13,17 @@
 showDoc = 0;
 
 %% Check if the compiler has been registered
-if exist('AR_Drone_Target/registry/thirdpartytools/thirdpartytools_win32.xml','file') == 0 || ...
-        exist('AR_Drone_Target/registry/thirdpartytools/thirdpartytools_win64.xml','file') == 0
+if exist('../registry/thirdpartytools/thirdpartytools_win32.xml','file') == 0 || ...
+        exist('../registry/thirdpartytools/thirdpartytools_win64.xml','file') == 0
     disp('No third party compiler has been registered for the AR Drone, running install script')
     showDoc = 1; % guess that since the compiler was not registered yet you will want to see the documentation
-   run('AR_Drone_Target/install_script.m') 
+   run('../install_script.m') 
 end
 
 %% Check if TargetRegistry has been set
-if exist('AR_Drone_Target/registry/gcc_codesourcery_arm_linux_gnueabihf_gmake_win64_v4_8.mat','file') == 0
+if exist('../registry/gcc_codesourcery_arm_linux_gnueabihf_gmake_win64_v4_8.mat','file') == 0
     disp('No toolchain info mat file was found for use with rtwTargetInfo.m, generating the gcc_codesourcery_arm_linux_gnueabihf_gmake_win64 mat file')
-    run('AR_Drone_Target\registry\generateTargetInfoMatFile.m');  % create the mat file containing the toolchain info
+    run('..\registry\generateTargetInfoMatFile.m');  % create the mat file containing the toolchain info
     RTW.TargetRegistry.getInstance('reset');         % reset the TargetRegistry such that rtwTargetInfo.m is called when a model is made for the first time
 end    
 
@@ -31,15 +31,15 @@ end
 sl_refresh_customizations;
 
 %% Compile the video library if needed
-if exist('AR_Drone_Target/blocks/videolib/AR_Drone_Video.slx','file') == 0
+if exist('../blocks/videolib/AR_Drone_Video.slx','file') == 0
     disp('No video library has been found, starting compilation of the video library using the Legacy Code Tool')
-   run('AR_Drone_Target/blocks/videolib/Generate_AR_Drone_Video.m')
+   run('../blocks/videolib/Generate_AR_Drone_Video.m')
 end
 
 %% Create the s functions for the AR_Drone_Library blocks
-if exist('AR_Drone_Target/blocks/rtwmakecfg.m','file') == 0 % the rtwmakecfg.m is created at the very end of Generate_AR_Drone_S_Functions
+if exist('../blocks/rtwmakecfg.m','file') == 0 % the rtwmakecfg.m is created at the very end of Generate_AR_Drone_S_Functions
     disp('No rtwmakecfg found for the blocks, running Generate_AR_Drone_S_Functions')
-   run('AR_Drone_Target/blocks/Generate_AR_Drone_S_Functions.m')
+   run('../blocks/Generate_AR_Drone_S_Functions.m')
 end
 
 %% set build folder
